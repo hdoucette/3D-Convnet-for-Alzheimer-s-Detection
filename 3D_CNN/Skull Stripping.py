8816a724eddec87504492152237656da1d860e1f
hillaryd <hillaryd@mit.edu>
2018-11-29 20:47:11 -0500

    attempt to overwrite stripped files

--- 3D_CNN/Skull Stripping.py
+++ 3D_CNN/Skull Stripping.py
@@ -4,26 +4,45 @@ import nipype.interfaces.fsl as fsl
 #import win32con
 from sys import platform
 
-if platform=='win32':
-    root='C:/Users/douce/Desktop/MIT Fall 2018/6.869 Machine Vision/Final Project/'
-else: root='/home/ubuntu'
+def skull_strip_all():
 
-file_path=os.path.join(root,'oasis-scripts/scans')
-for file in os.listdir(os.path.join(file_path)):
-    file_path_2=os.path.join(file_path,file)
-    #print(file)
-    for scan in os.listdir(file_path_2):
-        scan_path=os.path.join(file_path_2,scan)
-        #print(scan)
-        for image in os.listdir(scan_path):
-            if image.endswith('nii.gz'):
-                iFile=os.path.join(scan_path,image)
-                #print(iFile)
-                try:
-                    mybet = nipype.interfaces.fsl.BET(in_file=iFile,
+    if platform=='win32':
+        root='C:/Users/douce/Desktop/MIT Fall 2018/6.869 Machine Vision/Final Project/'
+    else: root='/home/ubuntu'
+
+    file_path=os.path.join(root,'oasis-scripts/scans')
+    for file in os.listdir(os.path.join(file_path)):
+        file_path_2=os.path.join(file_path,file)
+        #print(file)
+        for scan in os.listdir(file_path_2):
+            scan_path=os.path.join(file_path_2,scan)
+            #print(scan)
+            for image in os.listdir(scan_path):
+                if image.endswith('w.nii.gz'):
+                    iFile=os.path.join(scan_path,image)
+                    #print(iFile)
+                    try:
+                        mybet = nipype.interfaces.fsl.BET(in_file=iFile,
                                                       out_file=os.path.join(scan_path,image + '_stripped.nii'),
                                                       frac=0.5)
-                    mybet.run()  # executing the brain extraction
-                    print(file + ' is skull stripped')
-                except:
-                    print(file + ' is not skull stripped')
+                        mybet.run()  # executing the brain extraction
+                        print(file + ' is skull stripped')
+                    except:
+                        print(file + ' is not skull stripped')
+    return
+
+
+def skull_strip_single(img_path,dir):
+    imgFile = os.listdir(img_path)
+    try:
+        mybet = nipype.interfaces.fsl.BET(in_file=imgFile,
+                                          out_file=os.path.join(dir, imgFile + '_stripped.nii'),
+                                          frac=0.5)
+        mybet.run()  # executing the brain extraction
+        print(imgFile + ' is skull stripped')
+    except:
+        print(imgFile + ' is not skull stripped')
+
+    return
+
+skull_strip_all()
