17c3dfa8a685fd9f5588bc53a52e5dd8c8847db6
HillaryD <HillaryD@mit.edu>
2018-12-04 11:59:02 -0500

    Save data in zip npz file to save space

--- 3D_CNN/Final touches.py
+++ 3D_CNN/Final touches.py
@@ -33,82 +33,71 @@ labels_df=pd.read_csv(label_path,names=['path','patient_ID','diagnosis'])
 
 #labeling and object formation
 num=0
+netdata=[]
 for list in [train_list,test_list]:
     for file in list:
         path=file[0]
-        label=file[2]
-        netdata=[] #will be used for numpy object
+        label=file[1]
+        if int(float(label)) == 0:
+            labelar = np.array([1, 0, 0])
+        elif int(float(label)) >= 2:
+            labelar = np.array([0, 0, 1])
+        else:
+            labelar = np.array([0, 0, 1])
+        #netdata=[] #will be used for numpy object
         try:
             img = nibabel.load(path)  # loading the image
             img = img.get_data()
-            #img = skimage.transform.resize(img, (176, 256, 256), mode='constant')
+            img = skimage.transform.resize(img.astype(int), (176, 256, 256), mode='constant')
             num=num+1
-            if int(float(label)) == 0:
-                labelar = np.array([1, 0, 0])
-                netdata.append([img, labelar])
-                np.save(path, netdata)
-                print(num, 'of',denom," is npy saved")
-            elif int(float(label)) >=2:
-                labelar = np.array([0, 1, 0])
-                netdata.append([img, labelar])
-                np.save(path, netdata)
-                print(num, 'of',denom," is npy saved")
-            else:
-                labelar = np.array([0, 0, 1])
-                netdata.append([img, labelar])
-                np.save(path, netdata)
-                print(num, 'of',denom," is npy saved")
+            netdata.append([img, labelar,path])
+#             np.save(path, netdata)
+            print(num, 'of',denom," is transformed")
         except:
             print(path,' could not be appended and saved as numpy array')
-
-#normalization
+#
+# #normalization
 num=0
 totalnum=[] #total number of pixels in the image
 mean=[]  #mean of the pixels in the image
 nummax=[]  #maximum value of pixels in the image
 num=0
-for list in [train_list,test_list]:
-    for file in list:
-        file_name=file[0]+'.npy'
-        try:
-            img = np.load(file_name)
-            average=np.mean(img[0][0])
-            max=np.max(img[0][0])
-            size=img[0][0].shape[0]*img[0][0].shape[1]*img[0][0].shape[2]
-            mean.append(average)
-            totalnum.append(size)
-            nummax.append(max)
-            num=num+1
-            print(num," of",denom," appended to mean and max")
-        except:
-            print(file_name," could not be included in mean and max calculations")
 
-print(len(mean), len(totalnum))
+for item in netdata:
+    img = item[0]
+    average=np.mean(img)
+    max=np.max(img)
+    size=img.shape[0]*img.shape[1]*img.shape[2]
+    mean.append(average)
+    totalnum.append(size)
+    nummax.append(max.astype(float))
+    num=num+1
+    print(num," of",denom," appended to mean and max")
+
 nummean=np.vdot(mean,totalnum)/np.sum(totalnum)
 nummax=np.max(nummax)
 print('NUMMAX:',nummax,' NUMMEAN:',nummean)
 
 num=0
-for list in [train_list,test_list]:
-    for file in list:
-        try:
-            file_name=file[0]+'.npy'
-            img = np.load(file_name)
-            img[0][0]=(img[0][0]-nummean)/nummax #normalisation(x-mean/max value)
-            num+=1
-            print(num,' of',denom, ' is normalized')
-            np.save(file_name, img)
-        except:
-            print(file[0],' could not be normalized')
+for item in netdata:
+    img=item[0]
+    try:
+        img=(img-nummean)/nummax #normalisation(x-mean/max value)
+        item[0]=img
+        num+=1
+        print(num,' of',denom, ' is normalized')
+        np.savez_compressed(item[2],data=item)
+    except:
+        continue
 
-
-#Test Image Output
-# img = np.load(file_name)
-# img_data=img[0][0]
-# # print(img_data[100][100])
-# multi_slice_viewer(img_data)
+# Test Image Output
+# path=item[2]+'.npz'
+# print(path)
+# img_data=np.load(path)
+# img_data=img_data['data']
+# print(img_data.shape)
+# multi_slice_viewer(img_data[0])
 # plt.show()
-
 
 
 
